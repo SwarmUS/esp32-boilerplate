82928baccab24169eb6bc58e38daf4b11b740ff7
Gabriel Lessard <lesg2605@usherbrooke.ca>
2020-12-22 11:38:06 -0500

    CI additions: now runs clang and doxygen and builds esp target using a docker image

--- src/esp32/stub/src/cpu_start.c
+++ src/esp32/stub/src/cpu_start.c
@@ -1,12 +1,11 @@
-#include <stdbool.h>
-#include <setjmp.h>
 #include "esp_system.h"
+#include <setjmp.h>
+#include <stdbool.h>
 
 extern void app_main(void);
 jmp_buf buf;
 
-int main(void)
-{
+int main(void) {
     setjmp(buf);
     app_main();
 }
--- src/spi_flash/stub/src/flash_ops.c
+++ src/spi_flash/stub/src/flash_ops.c
@@ -1,6 +1,3 @@
 #include "../include/esp_spi_flash.h"
 
-int spi_flash_get_chip_size(void)
-{
-    return (1024 * 1024 * 1024);
-}
+int spi_flash_get_chip_size(void) { return (1024 * 1024 * 1024); }
