1c2f9096f1b5d37aabb9cc181293b23f91203808
Hornet <50376785+g-lesssard@users.noreply.github.com>
2021-01-04 10:13:43 -0500

    CI additions: now runs clang and doxygen and builds esp target using … (#2)
    * CI additions: now runs clang and doxygen and builds esp target using a docker image

--- src/freertos/stub/src/task.c
+++ src/freertos/stub/src/task.c
@@ -1,7 +1,4 @@
-#include <unistd.h>
 #include "freertos/task.h"
+#include <unistd.h>
 
-void vTaskDelay( const uint32_t xTicksToDelay )
-{
-    sleep(xTicksToDelay);
-}
+void vTaskDelay(const uint32_t xTicksToDelay) { sleep(xTicksToDelay); }
