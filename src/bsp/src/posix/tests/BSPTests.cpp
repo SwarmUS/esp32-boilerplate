af47867d6faf4279426e632129d6b3f25b65da71
Gabriel Lessard <lesg2605@usherbrooke.ca>
2021-01-10 14:19:43 -0500

    Moved Tests

---
+++ src/bsp/src/posix/tests/BSPTests.cpp
@@ -0,0 +1,26 @@
+#include <gtest/gtest.h>
+#include "BSP.h"
+#include <thread>
+
+class BspTests : public testing::Test {
+public:
+    BSP* m_bsp;
+
+    void SetUp() override {
+        m_bsp = new BSP();
+    }
+
+    void TearDown() override {
+        delete m_bsp;
+    }
+};
+
+TEST_F(BspTests, ValidateChipInfo) {
+    ChipInfo expectedChipInfo = {
+            .m_cores = (uint8_t) std::thread::hardware_concurrency(),
+            .m_osType = ChipInfo::SIMULATION
+    };
+    ASSERT_EQ(expectedChipInfo.m_cores, this->m_bsp->getChipInfo().m_cores);
+    ASSERT_EQ(expectedChipInfo.m_osType, this->m_bsp->getChipInfo().m_osType);
+}
+
