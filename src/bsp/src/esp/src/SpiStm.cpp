5f3c500e2b0b99ee6a19d83c7f7d8f5cb29c56f1
Gabriel Lessard <lesg2605@usherbrooke.ca>
2021-02-10 13:06:58 -0500

    Fixed crc8 for headers

--- src/bsp/src/esp/src/SpiStm.cpp
+++ src/bsp/src/esp/src/SpiStm.cpp
@@ -7,7 +7,6 @@
 #include <driver/gpio.h>
 
 
-static bool g_transTimeoutOut = false;
 void task(void* instance) {
     const uint8_t loopRate = 5;
     while (true) {
@@ -47,7 +46,7 @@ bool SpiStm::send(const uint8_t* buffer, uint16_t length) {
         // Memcpy necessary to have buffer word-alligned for transfer
         std::memcpy(m_outboundMessage.m_data.data(), buffer, length);
         // Padding with 0 up to a word-alligned boundary
-        for (uint8_t  i = 0; i < (length % 4); i++) {
+        for (uint8_t i = 0; i < (length % 4); i++) {
             m_outboundMessage.m_data[length] = 0;
             length++;
         }
@@ -77,14 +76,14 @@ void SpiStm::execute() {
     switch (m_rxState) {
     case receiveState::RECEIVING_HEADER:
         rxLengthBytes = sizeof(StmSpi::Header);
-        //m_logger.log(LogLevel::Error, "Receiving into header buffer");
         break;
     case receiveState::PARSING_HEADER:
         m_inboundHeader = (StmSpi::Header*)m_inboundMessage.m_data.data();
         if (m_inboundHeader->headerStruct.crc8 != CRC::calculateCRC8(m_inboundHeader, 3)) {
             m_logger.log(LogLevel::Error, "Received corrupted header");
-            m_logger.log(LogLevel::Info, "Bytes were: | %d | %d | %d | %d |", m_inboundMessage.m_data[0], m_inboundMessage.m_data[1], m_inboundMessage.m_data[2], m_inboundMessage.m_data[3]);
-            memset(m_inboundHeader, 0xFF, sizeof (m_outboundHeader));
+            m_logger.log(LogLevel::Info, "Bytes were: | %d | %d | %d | %d |",
+                         m_inboundMessage.m_data[0], m_inboundMessage.m_data[1],
+                         m_inboundMessage.m_data[2], m_inboundMessage.m_data[3]);
             m_rxState = receiveState::ERROR;
             break;
         }
@@ -122,7 +121,6 @@ void SpiStm::execute() {
         updateOutboundHeader();
         m_transaction.tx_buffer = &m_outboundHeader;
         txLengthBytes = sizeof(StmSpi::Header);
-        //m_logger.log(LogLevel::Info, "Output message contains header");
         break;
     case transmitState::SENDING_PAYLOAD:
         m_transaction.tx_buffer = m_outboundMessage.m_data.data();
@@ -140,14 +138,9 @@ void SpiStm::execute() {
     if (m_outboundMessage.m_sizeBytes != 0 && gpio_get_level(STM_CS) != 0) {
         notifyMaster();
     }
-    m_transaction.rx_buffer =  m_inboundMessage.m_data.data();
-    // This call is non-blocking, hence the TaskDelay
-    if (g_transTimeoutOut) {
-       m_logger.log(LogLevel::Info, "Transaction timeout before receiving everything");
-       g_transTimeoutOut = false;
-    }
-    spi_slave_queue_trans(STM_SPI, &m_transaction, m_loopRate);
+    m_transaction.rx_buffer = m_inboundMessage.m_data.data();
 
+    spi_slave_queue_trans(STM_SPI, &m_transaction, m_loopRate);
 }
 
 void SpiStm::updateOutboundHeader() {
@@ -155,9 +148,7 @@ void SpiStm::updateOutboundHeader() {
     if (m_inboundHeader != nullptr) {
         m_outboundHeader.headerStruct.rxSizeWord = m_inboundHeader->headerStruct.txSizeWord;
     }
-    uint8_t test[] = {0,3,4};
-    uint8_t crc = CRC::calculateCRC8(test, 3);
-    (void ) crc;
+
     m_outboundHeader.headerStruct.systemState.rawValue = 0; // TODO: get actual system state
 
     m_outboundHeader.headerStruct.crc8 = CRC::calculateCRC8(&m_outboundHeader, 3);
@@ -168,8 +159,7 @@ void SpiStm::notifyMaster() { gpio_set_level(STM_USER_0, 1); }
 void SpiStm::transactionCallback(void* instance, spi_slave_transaction_t* transaction) {
     auto* _this = static_cast<SpiStm*>(instance);
     if (transaction->length != transaction->trans_len) {
-        // Transaction timed out before it could finish. Nothing was received.
-        g_transTimeoutOut = true;
+        // Transaction timed out before it could finish.
         return;
     }
     switch (_this->m_rxState) {
