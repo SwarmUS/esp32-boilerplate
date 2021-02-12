72f3ea3d5c7ca1f55c6f064d5441684384b03a5f
Gabriel Lessard <lesg2605@usherbrooke.ca>
2021-02-11 19:44:33 -0500

    Fixed posix build

---
+++ src/bsp/src/posix/include/SpiStm.h
@@ -0,0 +1,20 @@
+#ifndef HIVE_CONNECT_SPISTM_H
+#define HIVE_CONNECT_SPISTM_H
+
+#include "bsp/ISpiStm.h"
+
+class SpiStm : public ISpiStm {
+  public:
+    SpiStm() = default;
+    ~SpiStm() override = default;
+
+    bool send(const uint8_t* buffer, uint16_t length) override {
+        (void)buffer;
+        (void)length;
+        return false;
+    };
+
+    bool isBusy() const override { return false; }
+};
+
+#endif // HIVE_CONNECT_SPISTM_H
