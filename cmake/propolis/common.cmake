38bac7649237059b972bd508f6224d7f062ff21e
Gabriel Lessard <lesg2605@usherbrooke.ca>
2021-02-22 19:42:02 -0500

    Include os from propolis

--- cmake/propolis/common.cmake
+++ cmake/propolis/common.cmake
@@ -13,7 +13,7 @@ function(propolis_fetch_populate)
         ${PROJECT_NAME}_propolis
 
         GIT_REPOSITORY https://github.com/SwarmUS/Propolis
-        GIT_TAG        2079e22
+        GIT_TAG        f185fce
         GIT_PROGRESS   TRUE
     )
 
