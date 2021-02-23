bef32a394337bc964165ccb11d1b0824bd37f1d6
Gabriel Lessard <lesg2605@usherbrooke.ca>
2021-02-22 19:56:43 -0500

    Added doc

--- src/bsp/src/esp/hal/spi_callbacks.h
+++ src/bsp/src/esp/hal/spi_callbacks.h
@@ -9,10 +9,20 @@ extern "C" {
 #include <driver/spi_slave.h>
 
 typedef void (*slaveTransactionCompleteCallback_t)(void* instance,
-                                                 spi_slave_transaction_t* transaction);
+                                                   spi_slave_transaction_t* transaction);
 
+/**
+ * @brief Function to be set on the configuration as the post transaction callback of the slave
+ * interface
+ * @param transaction Struct containing the transaction data
+ */
 void transactionCompleteCallback(spi_slave_transaction_t* transaction);
 
+/**
+ * @brief Function to set the callback for completed transaction
+ * @param cb Function to be called
+ * @param context Context passed to the callback function. Cannot be NULL
+ */
 void setCallback(slaveTransactionCompleteCallback_t cb, void* context);
 
 #ifdef __cplusplus
