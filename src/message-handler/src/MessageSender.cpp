dfd168857dd09700e70798bfd541406fae356059
Gabriel Lessard <lesg2605@usherbrooke.ca>
2021-03-19 15:48:50 -0400

    Bumped Propolis and implemented MessageSender

--- src/message-handler/src/MessageSender.cpp
+++ src/message-handler/src/MessageSender.cpp
@@ -1,4 +1,22 @@
-#include "IMessageSender.h"
-#include "ISwarmApiRequestHandler.h"
 #include "MessageSender.h"
-#include "SwarmApiRequestHandler.h"
+
+MessageSender::MessageSender(ICircularQueue<MessageDTO>& inputQueue, IHiveMindHostSerializer& serializer, IBSP& bsp, ILogger& logger) :
+   m_inputQueue(inputQueue), m_serializer(serializer), m_bsp(bsp), m_logger(logger) {}
+
+bool MessageSender::greet() {
+    uint16_t uuid = m_bsp.getUUID();
+    GreetingDTO greet(uuid);
+    MessageDTO msg(uuid, uuid, greet);
+    return m_serializer.serializeToStream(msg);
+}
+
+bool MessageSender::processAndSerialize() {
+    const std::optional<std::reference_wrapper<const MessageDTO>> message = m_inputQueue.peek();
+    if (message) {
+        bool ret = m_serializer.serializeToStream(message.value());
+        m_inputQueue.pop();
+        return ret;
+    }
+
+    return true;
+}
