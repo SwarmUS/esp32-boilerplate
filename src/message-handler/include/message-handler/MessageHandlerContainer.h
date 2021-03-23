61dc29d2dafe9a6a7e8451a87d55a3a29f49a9ed
Gabriel Lessard <lesg2605@usherbrooke.ca>
2021-03-23 19:38:00 -0400

    Added MessageHandlerContainer

--- src/message-handler/include/message-handler/MessageHandlerContainer.h
+++ src/message-handler/include/message-handler/MessageHandlerContainer.h
@@ -1,7 +1,41 @@
 #ifndef HIVE_CONNECT_MESSAGEHANDLERCONTAINER_H
 #define HIVE_CONNECT_MESSAGEHANDLERCONTAINER_H
 
-#include "M"
+#include "MessageDispatcher.h"
 #include "NetworkAPIHandler.h"
+#include "ThreadSafeQueue.h"
+
+namespace MessageHandlerContainer {
+
+/**
+ * @brief get the networkAPIHandler instance
+ * @return the instance of the networkApiHandler
+ */
+NetworkAPIHandler getNetworkApiHandler();
+
+/**
+ *@brief create a message dispatcher
+ *@return A new message dispatcher */
+MessageDispatcher createMessageDispatcher(IHiveMindHostDeserializer& deserializer,
+                                          INetworkAPIHandler& networkApiHandler);
+
+/**
+ * @brief get the Hivemind output queue
+ * @return a reference to the Hivemind output queue
+ */
+ThreadSafeQueue<MessageDTO>& getHivemindOutputQueue();
+
+/**
+ * @brief get the network unicast output queue
+ * @return a reference to the Hivemind output queue
+ */
+ThreadSafeQueue<MessageDTO>& getUnicastOutputQueue();
+
+/**
+ * @brief get the network broadcast output queue
+ * @return a reference to the Hivemind output queue
+ */
+ThreadSafeQueue<MessageDTO>& getBroadcastOutputQueue();
+} // namespace MessageHandlerContainer
 
 #endif // HIVE_CONNECT_MESSAGEHANDLERCONTAINER_H
