b723b65a902121ad0a94ca57fbb735f8f2aed1ec
Gabriel Lessard <lesg2605@usherbrooke.ca>
2021-03-30 13:17:32 -0400

    Added broker config in launch file

--- src/network/src/ros/src/broker/CommunicationBroker.cpp
+++ src/network/src/ros/src/broker/CommunicationBroker.cpp
@@ -15,7 +15,30 @@ CommunicationBroker::CommunicationBroker() {
     }
 }
 
-std::vector<uint16_t> CommunicationBroker::getRobotList() { return {}; }
+std::vector<uint16_t> CommunicationBroker::getRobotList() {
+    std::vector<uint16_t> robotList;
+    ros::NodeHandle nh("~");
+
+    XmlRpc::XmlRpcValue configRobotList;
+    std::string robotListParamName =
+        nh.param(std::string("configList"), std::string("/Broker/robots"));
+    if (!nh.getParam("/Broker/robots", configRobotList)) {
+        ROS_ERROR("No list was found");
+        return {};
+    }
+
+    if (configRobotList.getType() != XmlRpc::XmlRpcValue::TypeArray) {
+        ROS_ERROR("List not an array");
+        return {};
+    }
+
+    for (int i = 0; i < configRobotList.size(); i++) {
+        robotList.push_back((int)configRobotList[i]);
+    }
+    ROS_INFO("Subscribing to %zu agents", robotList.size());
+
+    return robotList;
+}
 
 void CommunicationBroker::communicationCallback(const hive_connect::Broadcast& msg) {
     for (auto& pair : m_publishersMap) {
