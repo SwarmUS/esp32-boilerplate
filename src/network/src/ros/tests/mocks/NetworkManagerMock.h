84d392ddfa27a611b37b2e882d06bf2844ad44db
Gabriel Lessard <lesg2605@usherbrooke.ca>
2021-04-02 08:44:42 -0400

    Fixed mock

--- src/network/src/ros/tests/mocks/NetworkManagerMock.h
+++ src/network/src/ros/tests/mocks/NetworkManagerMock.h
@@ -9,8 +9,8 @@ class NetworkManagerMock : public IAbstractNetworkManager {
   public:
     NetworkManagerMock() = default;
     MOCK_METHOD(void, start, (), (override));
-    MOCK_METHOD(NetworkStatus, getNetworkStatus, (), (override));
-    MOCK_METHOD(uint32_t, getSelfIP, (), (override));
+    MOCK_METHOD(NetworkStatus, getNetworkStatus, (), (const, override));
+    MOCK_METHOD(uint32_t, getSelfIP, (), (const, override));
     MOCK_METHOD(std::optional<uint32_t>, getIPFromAgentID, (uint16_t agentID), (const override));
     MOCK_METHOD(bool, registerAgent, (uint16_t agentID, uint32_t ip), (override));
 };
