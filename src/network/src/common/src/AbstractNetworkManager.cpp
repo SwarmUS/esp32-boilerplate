90cb20ad6dccaa7c2d6e9b2eca23c23812e8bf92
Gabriel Lessard <lesg2605@usherbrooke.ca>
2021-04-09 16:24:53 -0400

    Simplified to upsert

--- src/network/src/common/src/AbstractNetworkManager.cpp
+++ src/network/src/common/src/AbstractNetworkManager.cpp
@@ -2,18 +2,15 @@
 
 std::optional<uint32_t> AbstractNetworkManager::getIPFromAgentID(uint16_t agentID) const {
     auto agent = m_hashMap.at(agentID);
-    if (agent.has_value()) {
+    if (agent) {
         return agent.value().get();
     }
     return {};
 }
 
 bool AbstractNetworkManager::registerAgent(uint16_t agentID, uint32_t ip) {
-    if (m_hashMap.at(agentID).has_value() && m_hashMap.upsert(agentID, ip)) {
-        return true;
-    }
-    if (m_hashMap.insert(agentID, ip)) {
-        m_logger.log(LogLevel::Info, "Registered new agent %d with value %d", agentID, ip);
+    if (m_hashMap.upsert(agentID, ip)) {
+        m_logger.log(LogLevel::Debug, "Registered agent %d with value %d", agentID, ip);
         return true;
     }
     return false;
