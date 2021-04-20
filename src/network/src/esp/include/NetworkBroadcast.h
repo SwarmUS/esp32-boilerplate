e9170cc6da0614684a1de03863fa91be75cb5bef
Gabriel Lessard <lesg2605@usherbrooke.ca>
2021-04-20 14:16:10 -0400

    UDP goes brrrrrrrrr

--- src/network/src/esp/include/NetworkBroadcast.h
+++ src/network/src/esp/include/NetworkBroadcast.h
@@ -1,8 +1,11 @@
 #ifndef HIVE_CONNECT_NETWORKBROADCAST_H
 #define HIVE_CONNECT_NETWORKBROADCAST_H
 
+#include "BaseTask.h"
 #include "INetworkBroadcast.h"
+#include "c-common/circular_buff.h"
 #include "logger/ILogger.h"
+#include <array>
 
 class NetworkBroadcast : public INetworkBroadcast {
   public:
@@ -17,10 +20,17 @@ class NetworkBroadcast : public INetworkBroadcast {
 
     bool stop() override;
 
+    void receiveDatagrams();
+
   private:
     ILogger& m_logger;
+    BaseTask<2 * configMINIMAL_STACK_SIZE> m_receivingTask;
+    std::array<uint8_t, 512> m_data;
+    CircularBuff m_circularBuffer;
+    TaskHandle_t m_receivingTaskHandle;
     int m_socket;
     bool m_started;
+    std::array<uint8_t, 255> m_datagram;
 };
 
 #endif // HIVE_CONNECT_NETWORKBROADCAST_H
--- src/network/src/esp/src/NetworkBroadcast.cpp
+++ src/network/src/esp/src/NetworkBroadcast.cpp
@@ -1,10 +1,25 @@
 #include "NetworkBroadcast.h"
 #include "NetworkConfig.h"
 #include "SocketFactory.h"
+#include "Task.h"
 #include "lwip/sockets.h"
 
+static void task(void* context) {
+    while (true) {
+        static_cast<NetworkBroadcast*>(context)->receiveDatagrams();
+        Task::delay(20);
+    }
+}
+
 NetworkBroadcast::NetworkBroadcast(ILogger& logger) :
-    m_logger(logger), m_socket(NO_SOCKET), m_started(false) {}
+    m_logger(logger),
+    m_socket(NO_SOCKET),
+    m_started(false),
+    m_receivingTask("udp_receive", tskIDLE_PRIORITY, task, this) {
+    CircularBuff_init(&m_circularBuffer, m_data.data(), m_data.size());
+    m_receivingTaskHandle = nullptr;
+    m_receivingTask.start();
+}
 
 NetworkBroadcast::~NetworkBroadcast() { this->stop(); }
 
@@ -40,24 +55,14 @@ bool NetworkBroadcast::receive(uint8_t* data, uint16_t length) {
     if (data == nullptr || m_socket < 0) {
         return false;
     }
-    sockaddr_in broadcast;
-    broadcast.sin_addr.s_addr = htonl(INADDR_BROADCAST);
-    broadcast.sin_family = AF_INET;
-    broadcast.sin_port = htons(NetworkConfig::getBroadcastOutputPort());
-    broadcast.sin_len = sizeof(broadcast);
-    socklen_t socklen = sizeof(broadcast);
 
-    ssize_t receivedBytes = 0;
-    do {
-        ssize_t received = lwip_recv(m_socket, &data[receivedBytes], length - receivedBytes, 0);
-        if (received == -1) {
-            break;
-        }
-        receivedBytes += received;
+    m_receivingTaskHandle = xTaskGetCurrentTaskHandle();
+    while (CircularBuff_getLength(&m_circularBuffer) < length) {
+        ulTaskNotifyTake(pdTRUE, 500);
+    }
+    m_receivingTaskHandle = nullptr;
 
-    } while (receivedBytes != length);
-
-    return receivedBytes == length;
+    return CircularBuff_get(&m_circularBuffer, data, length) == length;
 }
 
 bool NetworkBroadcast::stop() {
@@ -67,4 +72,39 @@ bool NetworkBroadcast::stop() {
     m_socket = NO_SOCKET;
     m_started = false;
     return true;
+}
+
+void NetworkBroadcast::receiveDatagrams() {
+    if (m_started) {
+        sockaddr_in broadcast;
+        broadcast.sin_addr.s_addr = htonl(INADDR_BROADCAST);
+        broadcast.sin_family = AF_INET;
+        broadcast.sin_port = htons(NetworkConfig::getBroadcastOutputPort());
+        broadcast.sin_len = sizeof(broadcast);
+        socklen_t socklen = sizeof(broadcast);
+
+        // Receiving from a UDP socket de-queues the whole datagram from the receiving queue,
+        // regardless of the size read. Here, we receive up to a maximum of the size of m_datagram.
+        // The MSG_TRUNC flag will make the lwip_recvfrom return the size of the first datagram in
+        // the queue, similar to a MSG_PEEK. We can know the length of the datagram from the size
+        // returned.
+        ssize_t sizeToReceive =
+            lwip_recvfrom(m_socket, m_datagram.data(), m_datagram.size(), MSG_TRUNC | MSG_PEEK,
+                          (sockaddr*)&broadcast, &socklen);
+
+        // Receive the datagram
+        if (lwip_recvfrom(m_socket, m_datagram.data(), sizeToReceive, 0, (sockaddr*)&broadcast,
+                          &socklen) == -1) {
+            m_logger.log(LogLevel::Error, "Failed to receive bytes from UDP socket");
+            return;
+        }
+
+        if (CircularBuff_getFreeSize(&m_circularBuffer) >= sizeToReceive) {
+            m_logger.log(LogLevel::Debug, "Received %d bytes from broadcast", sizeToReceive);
+            CircularBuff_put(&m_circularBuffer, m_datagram.data(), sizeToReceive);
+            if (m_receivingTaskHandle != nullptr) {
+                xTaskNotifyGive(m_receivingTaskHandle);
+            }
+        }
+    }
 }
