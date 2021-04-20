4187346e5180fa0a3ae046587d91aa8dc2da4c66
Gabriel Lessard <lesg2605@usherbrooke.ca>
2021-04-20 14:51:08 -0400

    Clean ups

--- src/network/src/esp/src/NetworkBroadcast.cpp
+++ src/network/src/esp/src/NetworkBroadcast.cpp
@@ -40,13 +40,14 @@ bool NetworkBroadcast::send(const uint8_t* data, uint16_t length) {
         return false;
     }
 
-    // Destination
-    sockaddr_in broadcast;
+    // Destination broadcast
+    sockaddr_in broadcast{};
     broadcast.sin_addr.s_addr = htonl(INADDR_BROADCAST);
     broadcast.sin_family = AF_INET;
     broadcast.sin_port = htons(NetworkConfig::getBroadcastOutputPort());
     broadcast.sin_len = sizeof(broadcast);
 
+    // Use lwip_sendto with udp since it is a connection-less protocol.
     return lwip_sendto(m_socket, data, length, 0, (sockaddr*)&broadcast, sizeof(broadcast)) ==
            length;
 }
@@ -76,25 +77,17 @@ bool NetworkBroadcast::stop() {
 
 void NetworkBroadcast::receiveDatagrams() {
     if (m_started) {
-        sockaddr_in broadcast;
-        broadcast.sin_addr.s_addr = htonl(INADDR_BROADCAST);
-        broadcast.sin_family = AF_INET;
-        broadcast.sin_port = htons(NetworkConfig::getBroadcastOutputPort());
-        broadcast.sin_len = sizeof(broadcast);
-        socklen_t socklen = sizeof(broadcast);
 
         // Receiving from a UDP socket de-queues the whole datagram from the receiving queue,
         // regardless of the size read. Here, we receive up to a maximum of the size of m_datagram.
         // The MSG_TRUNC flag will make the lwip_recvfrom return the size of the first datagram in
-        // the queue, similar to a MSG_PEEK. We can know the length of the datagram from the size
-        // returned.
+        // the queue, and MSG_PEEK insures we do not pop the datagram yet from the queue.
+        // TLDR, the following line gets the size of the first datagram in the socket queue.
         ssize_t sizeToReceive =
-            lwip_recvfrom(m_socket, m_datagram.data(), m_datagram.size(), MSG_TRUNC | MSG_PEEK,
-                          (sockaddr*)&broadcast, &socklen);
+            lwip_recv(m_socket, m_datagram.data(), m_datagram.size(), MSG_TRUNC | MSG_PEEK);
 
         // Receive the datagram
-        if (lwip_recvfrom(m_socket, m_datagram.data(), sizeToReceive, 0, (sockaddr*)&broadcast,
-                          &socklen) == -1) {
+        if (lwip_recv(m_socket, m_datagram.data(), sizeToReceive, 0) == -1) {
             m_logger.log(LogLevel::Error, "Failed to receive bytes from UDP socket");
             return;
         }
