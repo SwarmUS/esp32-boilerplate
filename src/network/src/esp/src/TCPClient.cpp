7de0e82fd08ec751664263fc62255a5b96e9c81d
Gabriel Lessard <lesg2605@usherbrooke.ca>
2021-04-29 10:38:01 -0400

    Can now reproduce

--- src/network/src/esp/src/TCPClient.cpp
+++ src/network/src/esp/src/TCPClient.cpp
@@ -21,33 +21,22 @@ bool TCPClient::setDestination(uint32_t address) {
 }
 
 bool TCPClient::send(const uint8_t* data, uint16_t length) {
-
     if (m_socketFd == NO_SOCKET) {
         m_logger.log(LogLevel::Error, "Trying to send message with no socket client socket set");
         return false;
     }
-    ssize_t sentBytes = lwip_send(m_socketFd, data, length, 0);
-
-    if (sentBytes < 0) {
-        m_logger.log(LogLevel::Error, "Failed to send data");
-        closesocket(m_socketFd);
-        m_socketFd = NO_SOCKET;
-        return false;
-    }
-
-    while (sentBytes < length) {
+    ssize_t sentBytes = 0;
+    do {
         ssize_t nbytes = lwip_send(m_socketFd, data + sentBytes, (length - sentBytes), 0);
 
         if (nbytes < 0) {
-
             m_logger.log(LogLevel::Error, "Error while sending data from socket");
             closesocket(m_socketFd);
             m_socketFd = NO_SOCKET;
             return false;
         }
         sentBytes += nbytes;
-    }
-
+    } while (sentBytes < length);
     return sentBytes == length;
 }
 
